3e3f31e7bbf80e0d076dfc09188c22c43eccdbea
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-08-21 12:16:22 +0700

    deploy: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://rufflogix.github.io",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
