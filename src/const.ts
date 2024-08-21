ecee157e26937293c65311282bc828de11646129
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-08-21 12:22:55 +0700

    chore: const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,91 @@
-const GITHUB_URL: string = 'https://github.com/rufflogix';
-const RUFFLOGIX_IMAGE: string = './images/rufflogix.png';
+export type CardProps = {
+  name: string;
+  description: string;
+  link: string;
+  image: string;
+  tags: string[];
+};
 
-export { GITHUB_URL, RUFFLOGIX_IMAGE };
+const GITHUB_URL: string = "https://github.com/rufflogix";
+const RUFFLOGIX_IMAGE_256: string = "/images/rufflogix256.png";
+const RUFFLOGIX_IMAGE_512: string = "/images/rufflogix512.png";
+const RUFFLOGIX_IMAGE_1024: string = "/images/rufflogix1024.png";
 
+const PROJECT_CARDS: CardProps[] = [
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+];
+
+const ACTIVITY_CARDS: CardProps[] = [
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+  {
+    name: "RuffLogix",
+    description:
+      "RuffLogix is a blockchain-based platform for the development of decentralized applications.",
+    link: GITHUB_URL,
+    image: RUFFLOGIX_IMAGE_256,
+    tags: ["blockchain", "decentralized", "applications"],
+  },
+];
+
+export {
+  GITHUB_URL,
+  RUFFLOGIX_IMAGE_256,
+  RUFFLOGIX_IMAGE_512,
+  RUFFLOGIX_IMAGE_1024,
+  PROJECT_CARDS,
+  ACTIVITY_CARDS,
+};
