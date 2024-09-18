a799e191882a6e7ee019c04c7ac68fb89cc9c728
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2024-09-18 13:17:26 +0700

    feat: mermaid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-
+import remarkMermaid from "remark-mermaidjs";
 import react from "@astrojs/react";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rufflogix.github.io",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [expressiveCode(), mdx(), sitemap(), tailwind(), react()],
   markdown: {
-    remarkPlugins: [remarkMath],
+    remarkPlugins: [remarkMath, remarkMermaid],
     rehypePlugins: [rehypeKatex],
   },
 });
