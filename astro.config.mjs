106b8c4dd5b789d145ce196c47f3affd380c99bf
Teejuta Sriwaranon <teejuta.sriwaranon@gmail.com>
2025-07-31 23:46:20 +0700

    fix: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rufflogix.github.io",
+  base: "/rufflogix.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
